907ce3f6a77e0e31830a3c9341da2a94e7d0da52
plizhelpme <21001082@myrp.edu.sg>
2022-07-05 15:39:25 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21001082/demomyprofile/MainActivity.java
@@ -0,0 +1,52 @@
+package sg.edu.rp.c346.id21001082.demomyprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etName;
+    EditText etGPA;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.editTextName);
+        etGPA = findViewById(R.id.editTextGPA);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        String strName = etName.getText().toString();
+        float gpa = Float.parseFloat(etGPA.getText().toString());
+
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+
+        SharedPreferences.Editor prefEdit = prefs.edit();
+
+        prefEdit.putString("name", strName);
+        prefEdit.putFloat("gpa", gpa);
+
+        prefEdit.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+
+        String strName = prefs.getString("name", "Yu Xuan");
+        float gpa = prefs.getFloat("gpa", 0);
+
+        etName.setText(strName);
+        etGPA.setText(gpa + "");
+    }
+}
